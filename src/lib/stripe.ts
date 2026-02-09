eed5a8b0f9e6eac04a356fb1c2ff025599daf203
tekjoe <joe@tekjoe.org>
2026-02-08 20:19:34 -0600

    more stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,4 +3,5 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "dummy_key", {
   apiVersion: "2026-01-28.clover",
   typescript: true,
+  httpClient: Stripe.createFetchHttpClient(),
 });
