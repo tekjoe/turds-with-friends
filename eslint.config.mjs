18d5fefaf7df5420090753436d4b4b85ce500cf4
tekjoe <joe@tekjoe.org>
2026-02-11 21:55:01 -0600

    turn off lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,11 @@ const eslintConfig = [
   {
     ignores: [".next/**", "out/**", "build/**", ".open-next/**", ".wrangler/**"],
   },
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
